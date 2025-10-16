9769e7603ca42951ca93ee1d22058d9ac7009e5f
shavellegau <shavellegautami@gmail.com>
2025-10-16 14:32:32 +0700

    history dihilangkan

--- app/src/main/java/com/example/map_umkm/OrdersFragment.kt
+++ app/src/main/java/com/example/map_umkm/OrdersFragment.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.fragment.app.commit
 
 class OrdersFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,20 +18,9 @@ class OrdersFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val viewPager = view.findViewById<androidx.viewpager2.widget.ViewPager2>(R.id.viewPager)
-        val tabLayout = view.findViewById<com.google.android.material.tabs.TabLayout>(R.id.tabLayout)
-
-        val adapter = OrdersPagerAdapter(this)
-        viewPager.adapter = adapter
-
-        // Hanya 1 tab: "Ongoing"
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = "Ongoing"
-        }.attach()
-    }
-
-    class OrdersPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-        override fun getItemCount(): Int = 1  // hanya 1 fragment
-        override fun createFragment(position: Int): Fragment = OngoingOrdersFragment()
+        // Langsung tampilkan fragment OngoingOrders di container
+        childFragmentManager.commit {
+            replace(R.id.ordersContainer, OngoingOrdersFragment())
+        }
     }
 }
